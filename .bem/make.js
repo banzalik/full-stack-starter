6241e97422c67883dd5b3b4125b37c7eb4500a1a
Alexander Boythenko <banzalik@gmail.com>
2012-12-06 11:54:47 +0200

    вернул common.blocks прописал его в level. js вынес bam-core библиотеки в отдельную директорию .bem/libs/

--- .bem/make.js
+++ .bem/make.js
@@ -13,12 +13,12 @@ MAKE.decl('Arch', {
     getLibraries: function() {
 
         return {
-            '.bem/bem-bl': {
+            '.bem/libs/bem-bl': {
                 type: 'git',
                 url: 'git://github.com/bem/bem-bl.git',
                 treeish: '0.3'
             },
-            '.bem/bemhtml' : {
+            '.bem/libs/bemhtml' : {
                 type: 'git',
                 url: 'git://github.com/bem/bemhtml.git'
             }
--- desktop.bundles/.bem/level.js
+++ desktop.bundles/.bem/level.js
@@ -6,9 +6,10 @@ exports.getConfig = function() {
 
     return BEM.util.extend(this.__base() || {}, {
         bundleBuildLevels: this.resolvePaths([
-            '../../.bem/bem-bl/blocks-common',
-            '../../.bem/bem-bl/blocks-desktop',
-            '../../.bem/bemhtml/common.blocks',
+            '../../.bem/libs/bem-bl/blocks-common',
+            '../../.bem/libs/bem-bl/blocks-desktop',
+            '../../.bem/libs/bemhtml/common.blocks',
+            '../../common.blocks',
             '../../desktop.blocks'
         ])
     });
